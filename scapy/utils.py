9de4781a28203fc07147383dd876ea6eabb910d2
Michael Farrell <micolous+git@gmail.com>
2019-03-18 16:44:26 +1100

    fixup: call wireshark correctly

--- scapy/utils.py
+++ scapy/utils.py
@@ -1422,17 +1422,27 @@ def wireshark(pktlist):
 
 @conf.commands.register
 def tcpdump(pktlist, dump=False, getfd=False, args=None,
-            prog=None, getproc=False, quiet=False, use_tempfile=None):
+            prog=None, getproc=False, quiet=False, use_tempfile=None,
+            read_stdin_opts=None):
     """Run tcpdump or tshark on a list of packets.
 
     When using ``tcpdump`` on OSX (``prog == conf.prog.tcpdump``), this uses a
     temporary file to store the packets. This works around a bug in Apple's
     version of ``tcpdump``: http://apple.stackexchange.com/questions/152682/
 
-    Otherwise, the packets are passed in stdin, with ``-r -``.
+    Otherwise, the packets are passed in stdin.
 
     This function can be explicitly enabled or disabled with the
     ``use_tempfile`` parameter.
+
+    When using ``wireshark``, it will be called with ``-ki -`` to start
+    immediately capturing packets from stdin.
+
+    Otherwise, the command will be run with ``-r -`` (which is correct for
+    ``tcpdump`` and ``tshark``).
+
+    This can be overridden with ``read_stdin_opts``. This has no effect when
+    ``use_tempfile=True``, or otherwise reading packets from a regular file.
 
 pktlist: a Packet instance, a PacketList instance or a list of Packet
          instances. Can also be a filename (as a string) or an open
@@ -1451,6 +1461,8 @@ use_tempfile: When set to True, always use a temporary file to store packets.
               When set to False, pipe packets through stdin.
               When set to None (default), only use a temporary file with
               ``tcpdump`` on OSX.
+read_stdin_opts: When set, a list of arguments needed to capture from stdin.
+                 Otherwise, attempts to guess.
 
 Examples:
 
@@ -1498,6 +1510,13 @@ u'64'
         # http://apple.stackexchange.com/questions/152682/
         use_tempfile = DARWIN and prog[0] == conf.prog.tcpdump
 
+    if read_stdin_opts is None:
+        if prog[0] == conf.prog.wireshark:
+            # Start capturing immediately (-k) from stdin (-i -)
+            read_stdin_opts = ["-ki", "-"]
+        else:
+            read_stdin_opts = ["-r", "-"]
+
     if pktlist is None:
         with ContextManagerSubprocess(_prog_name, prog[0]):
             proc = subprocess.Popen(
@@ -1529,7 +1548,7 @@ u'64'
     else:
         with ContextManagerSubprocess(_prog_name, prog[0]):
             proc = subprocess.Popen(
-                prog + ["-r", "-"] + (args if args is not None else []),
+                prog + read_stdin_opts + (args if args is not None else []),
                 stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE if dump or getfd else None,
                 stderr=open(os.devnull) if quiet else None,
